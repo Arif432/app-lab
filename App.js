803dea92d8e55547731c2d728bd43deeb323d104
arif432 <nawazarif147@gmail.com>
2023-02-24 17:01:08 +0500

    initial coimmit

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image, TextInput, Button, ImageBackground, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-slate-600">
+
+      <View className="bg-red-600 h-48">
+        <Image
+        className="w-40 h-28 mt-14 ml-28"
+        source={{uri: "https://th.bing.com/th/id/OIP.OF59vsDmwxPP1tw7b_8clQHaE8?pid=ImgDet&rs=1"}} />
+      </View>
+
+      <View className="bg-orange-500 justify-center h-96">
+          <TextInput className="bg-white m-10 h-10 p-2 text-lg rounded-lg" placeholder='enter name' />
+          <TextInput placeholder='enter password' secureTextEntry className="bg-white m-10 h-10 p-2 text-lg rounded-lg" />
+      </View>
+
+      <View className="bg-cyan-200 h-64">
+          <TouchableOpacity>
+            <Text className="text-lg mt-16 ml-40 text-purple-500">Sign in</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity >
+            <Text className="text-lg mt-4 ml-40 text-purple-500">Sign up</Text>
+          </TouchableOpacity>
+
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{jsx,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
